226c96fd69f90bb63b4aeedefa9088442ef05c81
Luna5379 <110072210+Luna5379@users.noreply.github.com>
2024-09-02 16:55:59 +0000

    did some initial research on cyber and what i could include in my chatbot and started the chatbot code

---
+++ cyhelp.py
@@ -0,0 +1,7 @@
+print("Hello! I'm CyHelp.")
+
+print("The field of Cybersecurity started in the 1970s when more and more information started being stored on computer systems and networks!")
+
+print("The CIA Triad is the model used to discuss cybersecurity.")
+
+print("Thanks for chatting with me, and I hope you learned something new!")
